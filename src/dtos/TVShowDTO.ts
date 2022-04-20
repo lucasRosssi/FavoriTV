4b54bba5f4cac055bf482070a2af28b2af7a38b5
lucasRosssi <lucasrm2305@gmail.com>
2022-04-20 18:50:19 -0300

    added new interface data

--- src/dtos/TVShowDTO.ts
+++ src/dtos/TVShowDTO.ts
@@ -1,14 +1,18 @@
 export interface TVShowDTO {
 	id: string;
 	name: string;
-	language?: string;
-	genres?: string[];
-	summary?: string;
-	status?: string;
-	premiered?: string;
-	ended?: string;
-	image?: {
+	language: string;
+	genres: string[];
+	summary: string;
+	status: string;
+	premiered: string;
+	ended: string;
+	image: null | {
 		medium: string;
 		original: string;
+	};
+	runtime: number;
+	rating: {
+		average: number;
 	};
 }
