8df4eb0ed5cadae5c9b900a185c02c1ed791b30c
lucasRosssi <lucasrm2305@gmail.com>
2022-04-20 13:44:08 -0300

    worked on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components/native';
 
-import { Container } from './styles';
+import { Feather } from '@expo/vector-icons';
+import { SearchBar } from '../SearchBar';
+
+import { Container, MenuButton } from './styles';
 
 export function Header() {
-	return <Container></Container>;
+	const theme = useTheme();
+
+	return (
+		<Container>
+			<MenuButton>
+				<Feather name="menu" size={RFValue(40)} color={theme.colors.shape} />
+			</MenuButton>
+
+			<SearchBar />
+		</Container>
+	);
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,17 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
 	width: 100%;
+	background-color: ${({ theme }) => theme.colors.primary};
+
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+
+	padding: ${getStatusBarHeight() + 15}px 20px 15px;
 `;
+
+export const MenuButton = styled.TouchableOpacity.attrs({
+	activeOpacity: 0.5,
+})``;
