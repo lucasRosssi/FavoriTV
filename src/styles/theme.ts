9da336606c945c4070add18646bd9939290af026
lucasRosssi <lucasrm2305@gmail.com>
2022-04-20 12:23:50 -0300

    created theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export default {
+	fonts: {
+		regular: 'Poppins_400Regular',
+		bold: 'Poppins_700Bold',
+	},
+	colors: {
+		attention: '#E62929',
+		success: '#5CD65A',
+		background: '#2E2E2E',
+		primary: '#4E4E4E',
+		secondary: '#E6D00E',
+		shape: '#F7F7F7',
+	},
+};
