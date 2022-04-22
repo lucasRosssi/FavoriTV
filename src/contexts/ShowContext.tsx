4a4a3b6cf6159b7b69b2b3f5380cfab5449772c1
lucasRosssi <lucasrm2305@gmail.com>
2022-04-21 21:09:09 -0300

    worked on show context

--- src/contexts/ShowContext.tsx
+++ src/contexts/ShowContext.tsx
@@ -1,8 +1,14 @@
-import React, { createContext, ReactNode, useState } from 'react';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { TVShowDTO } from '../dtos/TVShowDTO';
 
 interface ShowContextData {
 	showTitle: string;
+	favoriteShows: TVShowDTO[];
 	changeShowTitle: (title: string) => void;
+	addFavorites: (tvShow: TVShowDTO) => void;
+	removeFavorites: (id: string) => void;
 }
 
 interface ShowContextProps {
@@ -13,16 +19,52 @@ const ShowContext = createContext({} as ShowContextData);
 
 function ShowProvider({ children }: ShowContextProps) {
 	const [showTitle, setShowTitle] = useState('');
+	const [favoriteShows, setFavoriteShows] = useState<TVShowDTO[]>([]);
+
+	const favoritesStorageKey = '@tvshows:';
 
 	function changeShowTitle(title: string) {
 		setShowTitle(title);
 	}
 
+	async function addFavorites(tvShow: TVShowDTO) {
+		try {
+			setFavoriteShows([...favoriteShows, tvShow]);
+			await AsyncStorage.setItem(
+				favoritesStorageKey,
+				JSON.stringify([...favoriteShows, tvShow])
+			);
+		} catch (error) {
+			throw new Error('Failed to store data');
+		}
+	}
+
+	function removeFavorites(id: string) {
+		const newList = favoriteShows.filter((item) => item.id !== id);
+
+		setFavoriteShows(newList);
+	}
+
+	useEffect(() => {
+		async function loadFavorites() {
+			const storedFavorites = await AsyncStorage.getItem(favoritesStorageKey);
+
+			if (storedFavorites) {
+				setFavoriteShows(JSON.parse(storedFavorites) as TVShowDTO[]);
+			}
+		}
+
+		loadFavorites();
+	}, []);
+
 	return (
 		<ShowContext.Provider
 			value={{
 				showTitle,
+				favoriteShows,
 				changeShowTitle,
+				addFavorites,
+				removeFavorites,
 			}}
 		>
 			{children}
